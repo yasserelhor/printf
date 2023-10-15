7a1a955d980232e938c8dfe62f60541c7914786a
anasjwd <anasjawad7777@gmail.com>
2023-10-15 05:24:58 -0700

    task2

--- print_bin.c
+++ print_bin.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * binlen - number of digits of the binary representation of a number.
+ * @num: thr number.
+ *
+ * Return: the number of a digits.
+ */
 int binlen(unsigned int num)
 {
 	int len = 0;
@@ -12,6 +18,10 @@ int binlen(unsigned int num)
 	return (len);
 }
 
+/**
+ * putbin - print decimal number in binary.
+ * @num: the number.
+ */
 void putbin(unsigned int num)
 {
 	if (num == 0)
@@ -20,6 +30,12 @@ void putbin(unsigned int num)
 	_putchar(num % 2 + '0');
 }
 
+/**
+ * print_bin - print decimal number in binary.
+ * @args: list of arguments (to extract the num from it).
+ *
+ * Return: the number of digits.
+ */
 int print_bin(va_list args)
 {
 	unsigned int num;
